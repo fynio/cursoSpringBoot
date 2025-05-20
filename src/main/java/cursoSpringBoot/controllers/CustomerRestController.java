3a3db97ea38c7130a47a1de4e34f55279876d38e
Rodrigo Trejo Garcia <rodrigogarciatrejo89@gmail.com>
2025-05-20 17:07:47 -0600

    holi

--- src/main/java/cursoSpringBoot/controllers/CustomerRestController.java
+++ src/main/java/cursoSpringBoot/controllers/CustomerRestController.java
@@ -4,6 +4,8 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,6 +13,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+
 
 
 
@@ -52,5 +56,45 @@ public class CustomerRestController {
         customers.add(customer);
         return customer;
     }
+
+
+    @PutMapping("/clientes")
+    public Customer putCliente(@RequestBody Customer customer)
+    {
+        for(Customer c: customers )
+        {
+            if(c.getId() == customer.getId())
+            {
+                c.setName(customer.getName());
+                c.setUsername(customer.getUsername());
+                c.setPassword(customer.getPassword());
+                return c;
+            }
+        }
+        return null;
+    }
+
+
+
+    @DeleteMapping("/clientes/{username}")
+    public void deleteCliente(@PathVariable String username) {
+        customers.removeIf(customer ->
+        customer.getUsername().equalsIgnoreCase(username));
+    }
+
+    @DeleteMapping("/clientes/byid/{id}")
+    public Customer deleteClienteById(@PathVariable int id) {
+        for(Customer c: customers )
+        {
+            if(c.getId() == id)
+            {
+                customers.remove(c);
+                return c;
+            }
+        }
+        return null;
+    }
+
     
+
 }
